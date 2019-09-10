026f63af268adecb55a3018f3c8df699fda0b252
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-09 22:29:03 -0300

    ajuste do gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+const browserSync = require("browser-sync")
+
+const { series, dest, src } = require('gulp')
+
+function style() {
+    console.log("SCSS running...")
+    return src('./scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest('./css'))
+        .pipe(browserSync.stream())
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style)
+    gulp.watch('./**/*.html', style).on("change", browserSync.reload)
+    gulp.watch('./**/*.js', style).on("change", browserSync.reload)
+    
+}
+
+exports.default = series(style)
+exports.default = series(watch)
