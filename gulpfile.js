7fcd2966cd0627a99a2bc69b93c322a9693181aa
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-16 11:08:59 -0300

    design responsivo completo

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 const gulp = require("gulp")
 const sass = require("gulp-sass")
 const browserSync = require("browser-sync")
-const prefix = require("gulp-autoprefixer")
+const sourceMaps = require("gulp-sourcemaps")
 
 const { series, dest, src } = require('gulp')
 
 function style() {
-    console.log("SCSS running...")
+    console.log("SCSS...")
+
+    var sassdocOptions = {
+        dest: './sassdoc'
+      };
+
     return src('./scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        // .pipe(console.log(prefix))
-        .pipe(prefix())
-        .pipe(dest('./css'))
+        .pipe(sourceMaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sourceMaps.write())
         .pipe(browserSync.stream())
-}
+        .pipe(dest('./css'))
+}  
 
 function watch() {
     browserSync.init({
